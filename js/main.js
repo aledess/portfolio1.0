e36f6469aa9d52209e06ef96d4c36cd90820e927
Alessandro <alessandrodess@gmail.com>
2019-10-03 11:55:16 +0200

    final

---
+++ js/main.js
@@ -0,0 +1,43 @@
+// Select DOM Items
+const menuBtn = document.querySelector('.menu-btn');
+const menu = document.querySelector('.menu');
+const menuNav = document.querySelector('.menu-nav');
+const menuBranding = document.querySelector('.menu-branding');
+const navItems = document.querySelectorAll('.nav-item');
+// const subtitle = document.querySelector('.sm-heading');
+
+
+
+
+
+
+// Set Initial State Of Menu
+let showMenu = false;
+
+menuBtn.addEventListener('click', toggleMenu);
+
+function toggleMenu() {
+    if (!showMenu) {
+        menuBtn.classList.add('close');
+        menu.classList.add('show');
+        menuNav.classList.add('show');
+        menuBranding.classList.add('show');
+        navItems.forEach(item => item.classList.add('show'));
+        // subtitle.style.display = "none";
+
+
+        // Set Menu State
+        showMenu = true;
+    } else {
+        menuBtn.classList.remove('close');
+        menu.classList.remove('show');
+        menuNav.classList.remove('show');
+        menuBranding.classList.remove('show');
+        navItems.forEach(item => item.classList.remove('show'));
+        // subtitle.style.display = "block";
+
+
+        // Set Menu State
+        showMenu = false;
+    }
+}
